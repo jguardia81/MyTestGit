70aee1b6798f666da2733ddf656525511db4a917
jguardia81 <jean.guardia@orange.fr>
2017-02-19 11:17:14 +0100

    Create first class that displays message

---
+++ src/main/java/com/guardia/testgit/main/Application.java
@@ -0,0 +1,14 @@
+package com.guardia.testgit.main;
+
+public class Application {
+
+	public Application() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Salut ca marche");
+
+	}
+
+}
