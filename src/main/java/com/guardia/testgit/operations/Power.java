eb4ecc3b04d841483edaa3573625b468763f86b9
Jean GUARDIA (Lenovo) <jean.guardia@orange.fr>
2017-02-19 21:16:20 +0100

    add power function

---
+++ src/main/java/com/guardia/testgit/operations/Power.java
@@ -0,0 +1,12 @@
+package com.guardia.testgit.operations;
+
+public class Power {
+
+	public Power() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int doPower(int a, int b) {
+		return (int)Math.pow(a, b);
+	}
+}
