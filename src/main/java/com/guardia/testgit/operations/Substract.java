43b1aab36f0fa38ff3228f336b8768f60532effc
Jean GUARDIA (Lenovo) <jean.guardia@orange.fr>
2017-02-19 17:59:02 +0100

    Add substract operation. Add test case  for sub

---
+++ src/main/java/com/guardia/testgit/operations/Substract.java
@@ -0,0 +1,11 @@
+package com.guardia.testgit.operations;
+
+public class Substract {
+
+	public Substract() {
+		// TODO Auto-generated constructor stub
+	}
+	public int doSub(int a, int b) {
+		return a - b;
+	}
+}
