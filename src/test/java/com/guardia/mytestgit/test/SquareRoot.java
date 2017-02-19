e537ad03da7ef0f407a4ef59a13fd6b6adf979de
Jean GUARDIA (Lenovo) <jean.guardia@orange.fr>
2017-02-19 21:28:42 +0100

    Add square root and tests

---
+++ src/test/java/com/guardia/mytestgit/test/SquareRoot.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.guardia.mytestgit.test;
+
+/**
+ * @author jean
+ *
+ */
+public class SquareRoot {
+
+	/**
+	 * 
+	 */
+	public SquareRoot() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	double doSquareRoot(double a) {
+		return Math.sqrt(a);
+	}
+
+}
---
+++ src/test/java/com/guardia/mytestgit/test/TestSquareRoot.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.guardia.mytestgit.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * @author jean
+ *
+ */
+public class TestSquareRoot {
+     private SquareRoot squareRoot = new SquareRoot();
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		squareRoot = new SquareRoot();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.guardia.mytestgit.test.SquareRoot#SquareRoot()}.
+	 */
+	@Test
+	public void testSquareRoot() {
+		Assert.assertTrue(squareRoot != null);
+	}
+
+	/**
+	 * Test method for {@link com.guardia.mytestgit.test.SquareRoot#doSquareRoot(double)}.
+	 */
+	@Test
+	public void testDoSquareRoot() {
+		Assert.assertEquals(squareRoot.doSquareRoot(9), 3.0);
+	}
+
+}
